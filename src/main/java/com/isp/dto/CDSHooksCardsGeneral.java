a384d7d55cf05fe1e22d88be87b8808ab26e9ccb
vkoschman <207955@niuitmo.ru>
2019-12-06 02:10:13 +0300

    id made atomicInteger

--- src/main/java/com/isp/dto/CDSHooksCardsGeneral.java
+++ src/main/java/com/isp/dto/CDSHooksCardsGeneral.java
@@ -1,10 +1,11 @@
 package com.isp.dto;
 
 import java.util.Collections;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CDSHooksCardsGeneral {
     private static int questionnaireId = 0;
-    private static int linkId = 0;
+    private static AtomicInteger linkId = new AtomicInteger(0);
 
     public static String stringQuestionnaireId() {
         return String.format("qId%s", String.valueOf(questionnaireId++));
@@ -12,7 +13,7 @@ public class CDSHooksCardsGeneral {
 
     public static String stringLinkId() {
         // TODO atomic variable
-        return String.format("lId%s", String.valueOf(linkId++));
+        return String.format("lId%s", String.valueOf(linkId.getAndAdd(1)));
     }
     public static Cards cardsResponse(String summary, String detail) {
         return new Cards(Collections.singletonList(new Card(summary, detail)));
